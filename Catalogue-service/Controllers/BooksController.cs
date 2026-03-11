a5877ed766daa81320f9b38c3929021ab43bed9a
Catalogue API <josefine.lindbom-olsson@student.hv.se>
2026-03-11 13:17:31 +0100

    Startat projekt

---
+++ Catalogue-service/Controllers/BooksController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalogue_service.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BooksController : ControllerBase
+{
+    
+}
---
+++ Catalogue-service/Models/Books.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalogue_service.Models;
+
+public class Books
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Publisher { get; set; }
+    public int Year { get; set; }
+}
