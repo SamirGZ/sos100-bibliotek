aa22cb78539b710ed77cef05ec04fcad27b38a50
Samir Gulzad <gulzadsamir@gmail.com>
2026-03-15 20:35:51 +0100

    Added Login Controller and fixed some CSS issues

--- sos100-bibliotek/Program.cs
+++ sos100-bibliotek/Program.cs
@@ -3,13 +3,30 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddHttpClient<UserApiService>(client =>
+    {
+        client.BaseAddress = new Uri(builder.Configuration["ApiSettings:UserApiBaseUrl"]!);
+    }).ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    });
+}
+else
+{
+    builder.Services.AddHttpClient<UserApiService>(client =>
+    {
+        client.BaseAddress = new Uri(builder.Configuration["ApiSettings:UserApiBaseUrl"]!);
+    });
+}
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -25,6 +42,4 @@ app.MapControllerRoute(
         pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
-
 app.Run();
-Console.Write("Hello World");
