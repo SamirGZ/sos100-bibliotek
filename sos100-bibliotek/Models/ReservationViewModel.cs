5085ca7f4367c19b2ff3a3309bb99e6e8e43aae7
Sabrin Saleman <sabrinsaleman12@gmail.com>
2026-03-25 04:15:37 +0100

    synced ReservationViewModel.cs with reservation model

--- sos100-bibliotek/Models/ReservationViewModel.cs
+++ sos100-bibliotek/Models/ReservationViewModel.cs
@@ -3,8 +3,12 @@ namespace sos100_bibliotek.Models;
 public class ReservationViewModel
 {
     public int Id { get; set; }
-    public int ItemId { get; set; }
+
+    public int BookId { get; set; }
+
     public int UserId { get; set; }
+
     public DateTime ReservationDate { get; set; }
-    public string Status { get; set; } = string.Empty;
+
+    public string Status { get; set; } = "Active";
 }
