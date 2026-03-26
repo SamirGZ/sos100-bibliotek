da0aff11e73bb265775c485cfeb357d519f0df64
Sabrin Saleman <sabrinsaleman12@gmail.com>
2026-03-26 23:58:10 +0100

    lägg till boktitel och författare i reservation view model

--- sos100-bibliotek/Models/ReservationViewModel.cs
+++ sos100-bibliotek/Models/ReservationViewModel.cs
@@ -7,4 +7,7 @@ public class ReservationViewModel
     public int UserId { get; set; }
     public DateTime ReservationDate { get; set; }
     public string Status { get; set; } = string.Empty;
+    
+    public string BookTitle { get; set; } = "";
+    public string BookAuthor { get; set; } = "";
 }
