ac46afb24d5b94dafb750133d556bd9bf70b484d
Sabrin Saleman <sabrinsaleman12@gmail.com>
2026-03-26 04:29:25 +0100

    Revert "Change titles"
    This reverts commit 2429b022901c164fd0256b491a42e88914e40ba6.

--- ReservationAPI/Models/reservation.cs
+++ ReservationAPI/Models/reservation.cs
@@ -4,13 +4,11 @@ public class Reservation
 {
     public int Id { get; set; }
 
-    // Koppling till bok i katalogen
-    public int BookId { get; set; }
+    public int ItemId { get; set; }
 
-    // Koppling till användaren som reserverar
     public int UserId { get; set; }
 
-    public DateTime ReservationDate { get; set; } = DateTime.Now;
+    public DateTime ReservationDate { get; set; }
 
     public string Status { get; set; } = "Active";
 }
