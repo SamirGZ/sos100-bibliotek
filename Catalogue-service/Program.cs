e1d5e79547fe41340c73950c127bc3d5b08e9416
Catalogue API <josefine.lindbom-olsson@student.hv.se>
2026-05-20 13:00:29 +0200

    Fixat CORS-policy

--- Catalogue-service/Program.cs
+++ Catalogue-service/Program.cs
@@ -15,6 +15,16 @@ builder.Services.AddDbContext<BookCatalogueDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ReactAppPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,6 +35,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("ReactAppPolicy");
 
 app.UseAuthorization();
 
